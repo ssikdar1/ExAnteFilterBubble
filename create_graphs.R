d35cbd50d44b3dee766215173df12a8010a319ae
ssikdar-r7 <shan_sikdar@rapid7.com>
2019-04-16 13:40:04 -0400

    add R script

--- create_graphs.R
+++ create_graphs.R
@@ -16,9 +16,9 @@ get_mean_diversity <- function(df) {
 }
 
 # like above but get mean std, n , confidence intervals for graph
-get_stats_diversity <- function(rec_data, rho_rate){
+get_stats_diversity <- function(rec_data, beta_rate){
   df <- rec_data %>% 
-    filter(rho == rho_rate) %>%
+    filter(beta == beta_rate) %>%
     group_by(regime, pop_idx, rho, beta) %>% 
     mutate(diversity = mean(diversity_score)) %>%
     slice(1) %>%
@@ -34,13 +34,13 @@ get_stats_diversity <- function(rec_data, rho_rate){
 
 
 # graph w/ themes
-graph_stats_diversity <- function(df,rho){
-  g <- ggplot(df2, aes(x=beta, y=diversity_mean)) +
+graph_stats_diversity <- function(df,beta){
+  g <- ggplot(df2, aes(x=rho, y=diversity_mean)) +
     geom_line(aes(colour=regime)) +
     geom_errorbar(aes(ymin=lower_ci_diversity, ymax=upper_ci_diveristy), width=.02,
                   position=position_dodge(.9)) + 
-    labs(x="beta", y="diversity",
-         title=paste("Rho=",rho,"N=500,T=25, Diversity",sep=" "),
+    labs(x="rho", y="average diversity",
+         title=paste("Beta=",beta,"N=500,T=25, Diversity",sep=" "),
          #subtitle="A plot that is only useful for demonstration purposes",
          caption="Columbia University") + 
     theme_ipsum_rc()
@@ -57,15 +57,15 @@ get_mean_welfare <- function(df) {
   return(d)
 }
 
-get_stats_welfare <- function(rec_data, rho_rate){
+get_stats_welfare <- function(rec_data, beta_rate){
   df <- rec_data %>% 
-    filter(rho == rho_rate) %>%
+    filter(beta == beta_rate) %>%
     group_by(regime, pop_idx, rho, beta) %>% 
     mutate(welfare_calc = mean(welfare)) %>%
     slice(1) %>%
     group_by(regime, rho, beta) %>% 
     mutate(welfare_mean = mean(welfare_calc),
-          welfare_sd = sd(welfare_calc),
+           welfare_sd = sd(welfare_calc),
            welfare_n = n()) %>% 
     mutate(welfare_se =  welfare_sd/ sqrt(welfare_n),
            lower_ci_welfare = welfare_mean - qt(1 - (0.05 / 2), welfare_n - 1) * welfare_se,
@@ -74,34 +74,38 @@ get_stats_welfare <- function(rec_data, rho_rate){
 }
 
 # graph w/ themes
-graph_stats_welfare <- function(d,rho){
-  g <- ggplot(d, aes(x=beta, y=welfare_mean)) +
+graph_stats_welfare <- function(d,beta){
+  g <- ggplot(d, aes(x=rho, y=welfare_mean)) +
     geom_line(aes(colour=regime)) +
     geom_errorbar(aes(ymin=lower_ci_welfare, ymax=upper_ci_welfare), width=.02, position=position_dodge(.9)) + 
-    labs(x="beta", y="welfare",
-         title=paste("Rho=",rho,"N=500,T=25, Welfare",sep=" "),
+    labs(x="rho", y="average welfare",
+         title=paste("Beta=",beta,"N=500,T=25, Welfare",sep=" "),
          #subtitle="A plot that is only useful for demonstration purposes",
          caption="Columbia University") + 
     theme_ipsum_rc()
   return(g)
 }
 
+
+g <- graph_stats_welfare(get_stats_welfare(rec_data,.9), .9)
+g
+
 #df2 <- get_stats_diversity(rec_data, 0.9)
 #g <- graph_stats_diversity(df2)
 #ggsave(filename=paste(WORKING_DIR, "figures/rho_09_diversity.jpeg", sep=""), plot=g)
 
 
-for (rho in c(0.9, 0.5, 0.1)){
+for (beta in c(0.9, 0.5, 0.1)){
   # diversity
-  rho_name <- paste("rho_", rho, sep="")
-  print(rho_name)
-  df2 <- get_stats_diversity(rec_data, rho)
-  g <- graph_stats_diversity(df2,rho)
-  ggsave(filename=paste(WORKING_DIR, "figures2/", rho_name, "_diversity.jpeg", sep=""), plot=g)
+  beta_name <- paste("beta_", beta, sep="")
+  print(beta_name)
+  df2 <- get_stats_diversity(rec_data, beta)
+  g <- graph_stats_diversity(df2,beta)
+  ggsave(filename=paste(WORKING_DIR, "figures2/", beta_name, "_diversity.jpeg", sep=""), plot=g)
   
   #welfare
-  g <- graph_stats_welfare(get_stats_welfare(rec_data,rho), rho)
-  ggsave(filename=paste(WORKING_DIR, "figures2/", rho_name, "_welfare.jpeg", sep=""), plot=g)
+  g <- graph_stats_welfare(get_stats_welfare(rec_data,beta), beta)
+  ggsave(filename=paste(WORKING_DIR, "figures2/", beta_name, "_welfare.jpeg", sep=""), plot=g)
   
 }
 
