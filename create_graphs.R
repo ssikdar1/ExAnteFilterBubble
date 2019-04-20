4872f2823eec169566ff3662bb1d1bc45aebf293
Guy Aridor <garidor@gmail.com>
2019-04-20 10:24:23 -0400

    paper

--- create_graphs.R
+++ create_graphs.R
@@ -57,7 +57,7 @@ graph_stats_diversity <- function(df, var, use_hrbrthemes){
   N <- df$N[1]
   
   g <- ggplot(df, aes_string(x=var, y="diversity_mean")) +
-      geom_line(aes(colour=regime)) +
+      geom_line(aes(colour=formatted_regime)) +
       geom_errorbar(aes(ymin=lower_ci_diversity, ymax=upper_ci_diveristy), width=.02,
                     position=position_dodge(.9)) + 
       labs(x=var, y="diversity",
@@ -89,7 +89,7 @@ graph_stats_welfare <- function(d, var, use_hrbrthemes){
   T_val <- d$T[1]
   N <- d$N[1]
   g <- ggplot(d, aes_string(x=var, y="welfare_mean")) +
-    geom_line(aes(colour=regime)) +
+    geom_line(aes(colour=formatted_regime)) +
     geom_errorbar(aes(ymin=lower_ci_welfare, ymax=upper_ci_welfare), width=.02, position=position_dodge(.9)) + 
     labs(x=var, y="welfare",
          title=paste("N =", N, "T =", T_val, "Welfare",sep=" ")) 
@@ -133,31 +133,15 @@ get_stats_welfare <- function(rec_data, var){
 ## HOMOGENEITY DATA
 
 get_stats_homogeneity <- function(stats_df, var){
-  if (var == "rho") { # there must be a better way
-    df <- stats_df %>%
-      group_by(regime, rho) %>% 
-      mutate(jaccard_mean = mean(jaccard),
-             jaccard_sd = sd(jaccard),
-             jaccard_n = n()) %>%
-      mutate(jaccard_se =  jaccard_sd/ sqrt(jaccard_n),
-             lower_ci_jaccard = jaccard_mean - qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se,
-             upper_ci_jaccard = jaccard_mean + qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se) %>% 
-      slice(1)
-    return(df)
+  df <- stats_df
+  if (var == "rho") {
+    df <- df %>% group_by(regime, rho) 
   } else if (var == "beta") {
-    df <- stats_df %>%
-      group_by(regime, beta) %>% 
-      mutate(jaccard_mean = mean(jaccard),
-             jaccard_sd = sd(jaccard),
-             jaccard_n = n()) %>%
-      mutate(jaccard_se =  jaccard_sd/ sqrt(jaccard_n),
-             lower_ci_jaccard = jaccard_mean - qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se,
-             upper_ci_jaccard = jaccard_mean + qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se) %>% 
-      slice(1)
-    return(df)
+    df <- df %>% group_by(regime, beta) 
   } else if (var == "sigma") {
-    df <- stats_df %>%
-      group_by(regime, sigma) %>% 
+    df <- df %>% group_by(regime, sigma) 
+  }
+  df <- df %>%
       mutate(jaccard_mean = mean(jaccard),
              jaccard_sd = sd(jaccard),
              jaccard_n = n()) %>%
@@ -165,17 +149,17 @@ get_stats_homogeneity <- function(stats_df, var){
              lower_ci_jaccard = jaccard_mean - qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se,
              upper_ci_jaccard = jaccard_mean + qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se) %>% 
       slice(1)
-    return(df)
-  }
+  return(df)
 }
 
 
 graph_stats_homo <- function(df, var){
   T_val <- df$T[1]
   N <- df$N[1]
+  
   if (var == "rho") {
     g <- ggplot(df, aes(x=rho, y=jaccard_mean)) +
-      geom_line(aes(colour=regime)) +
+      geom_line(aes(colour=formatted_regime)) +
       geom_errorbar(aes(ymin=lower_ci_jaccard, ymax=upper_ci_jaccard), width=.02,
                     position=position_dodge(.9)) + 
       labs(x="rho", y="homogeneity",
@@ -183,7 +167,7 @@ graph_stats_homo <- function(df, var){
     return(g)
   } else if (var == "beta") {
     g <- ggplot(df, aes(x=beta, y=jaccard_mean)) +
-      geom_line(aes(colour=regime)) +
+      geom_line(aes(colour=formatted_regime)) +
       geom_errorbar(aes(ymin=lower_ci_jaccard, ymax=upper_ci_jaccard), width=.02,
                     position=position_dodge(.9)) + 
       labs(x="beta", y="homogeneity",
@@ -191,7 +175,7 @@ graph_stats_homo <- function(df, var){
     return(g)
   } else if (var == "sigma") {
     g <- ggplot(df, aes(x=sigma, y=jaccard_mean)) +
-      geom_line(aes(colour=regime)) +
+      geom_line(aes(colour=formatted_regime)) +
       geom_errorbar(aes(ymin=lower_ci_jaccard, ymax=upper_ci_jaccard), width=.02,
                     position=position_dodge(.9)) + 
       labs(x="sigma", y="homogeneity",
@@ -200,11 +184,14 @@ graph_stats_homo <- function(df, var){
   }
 }
 
-#WORKING_DIR <- "/Users/guyaridor/Desktop/recommender_systems/rec_sys_conf_paper/"
-WORKING_DIR <- "/Users/ssikdar/Downloads/econ/bubbles/"
+WORKING_DIR <- "/Users/guyaridor/Desktop/recommender_systems/rec_sys_conf_paper/"
+#WORKING_DIR <- "/Users/ssikdar/Downloads/recommender_systems/rec_sys_conf_paper"
 
-rec_data <- read.csv(paste(WORKING_DIR, "rec_data_t_15.csv", sep=""))
-homogeneity <- read.csv(paste(WORKING_DIR, "homogeneity_data_t_15.csv", sep=""))
+rec_data <- read.csv(paste(WORKING_DIR, "rec_data_t_25.csv", sep=""))
+rec_data <- rec_data %>% mutate(formatted_regime = ifelse(regime == "rec", "Omniscient", ifelse(regime == "no_rec", "No Rec", "Partial")))
+
+homogeneity <- read.csv(paste(WORKING_DIR, "homogeneity_data_t_25.csv", sep=""))
+homogeneity <- homogeneity%>% mutate(formatted_regime = ifelse(regime == "rec", "Omniscient", ifelse(regime == "no_rec", "No Rec", "Partial")))
 
 
 # CREATE GRAPHS
@@ -218,21 +205,21 @@ for(metric in metrics){
   for(variable in variables){
     print(variable)
     
-    file_name <- paste(WORKING_DIR,"figures/",variable,"_", metric, "_t_15.jpeg", sep="")
+    file_name <- paste(WORKING_DIR,"figures/",variable,"_", metric, "_t_25.jpeg", sep="")
     print(file_name)
     
     if (metric == "diversity"){
-      g <- graph_stats_diversity(get_stats_diversity(rec_data, variable), variable)
+      g <- graph_stats_diversity(get_stats_diversity(rec_data, variable), variable, use_hrbrthemes)
       ggsave(filename=file_name, plot=g)
     }
     else if (metric == "welfare"){
       #welfare
-      g <- graph_stats_welfare(get_stats_welfare(rec_data, variable), variable)
+      g <- graph_stats_welfare(get_stats_welfare(rec_data, variable), variable,  use_hrbrthemes)
       ggsave(filename=file_name, plot=g)
       
-      rec_file_name <- paste(WORKING_DIR, "figures/", variable, "_rec_t_15.jpeg", sep="")
+      rec_file_name <- paste(WORKING_DIR, "figures/", variable, "_rec_t_25.jpeg", sep="")
       print(rec_file_name)
-      g <- graph_stats_rec(get_stats_rec(filter(rec_data, regime == "partial"), variable), variable)
+      g <- graph_stats_rec(get_stats_rec(filter(rec_data, regime == "partial"), variable), variable,  use_hrbrthemes)
       ggsave(filename=rec_file_name, plot=g)
     } 
     else{
@@ -243,21 +230,97 @@ for(metric in metrics){
   }
 }
 
-scatter <- function(df, var_x, var_y, use_hrbrthemes){
+scatter <- function(df, var_x, var_y, use_hrbrthemes, title){
   g <- ggplot(df, aes_string(var_x, var_y)) +
-         stat_summary_bin(fun.y = "mean", geom = "bar")
-    labs(x=var_x, y=var_y,
-         title=paste(var_x," x ",var_y, sep = "") #,
+         geom_smooth() + #stat_summary_bin(fun.y = "mean", geom="point", bins = 10) +
+    labs(x=var_x, y=var_y
          #subtitle="A plot that is only useful for demonstration purposes",
          #caption="Brought to you by the letter 'g'"
          )
   if(use_hrbrthemes){
-    g <- g + theme_ipsum_rc()
+    g <- g + theme_ipsum_rc(axis_title_size = 14,plot_title_size = 24)
   }
   return(g)
 }
 
+rec_abbrv_to_name <- function(abbrv) {
+  if (abbrv == "rec") {
+    return("Omniscient")
+  } else if (abbrv == "no_rec") {
+    return("No Recommendation")
+  } else if (abbrv == "partial") {
+    return("Partial")
+  }
+}
+
+rec_policies <- c("rec", "no_rec", "partial")
+for (rec_policy in rec_policies) {
+  title <- paste("Diversity vs Welfare -", rec_abbrv_to_name(rec_policy))
+  ggsave(
+    filename=paste(WORKING_DIR, "figures/", title, ".jpeg", sep=""), 
+    plot=scatter(filter(rec_data, regime == rec_policy), "diversity_score", "welfare", use_hrbrthemes, title)
+  )
+}
+
+df <- rec_data %>% group_by(regime) %>%
+  mutate(welfare_mean = mean(welfare),
+         welfare_sd = sd(welfare),
+         welfare_n = n(),
+         diversity_mean = mean(diversity_score),
+         diversity_sd = sd(diversity_score),
+         diversity_n = n()) %>% 
+  mutate(welfare_se =  welfare_sd/ sqrt(welfare_n),
+         welfare_mul = qt(1 - (0.05 / 2), welfare_n - 1) * welfare_se,
+         welfare_str = paste(round(welfare_mean, 2), "\\pm", round(welfare_mul, 4))) %>%
+  mutate(diversity_se  =  diversity_sd/ sqrt(diversity_n),
+          diversity_mul = qt(1 - (0.05 / 2), diversity_n - 1) * diversity_se,
+          diversity_str = paste(round(diversity_mean, 2), "\\pm", round(diversity_mul, 4))) %>%
+  slice(1)
+
+df <- df[,c("regime", "welfare_str", "diversity_str")]
+
+homo <- homogeneity %>% group_by(regime) %>%
+  mutate(jaccard_mean = mean(jaccard),
+         jaccard_sd = sd(jaccard),
+         jaccard_n = n()) %>%
+  mutate(jaccard_se  =  jaccard_sd/ sqrt(jaccard_n),
+         jaccard_mul = qt(1 - (0.05 / 2), jaccard_n - 1) * jaccard_se,
+         jaccard_str = paste(round(jaccard_mean, 3), "\\pm", round(jaccard_mul, 4))) %>%
+  slice(1)
+homo <- homo[,c("regime", "jaccard_str")]
+
+df <- merge(df, homo, by="regime")
+library(xtable)
+xtable(df)
+
+time_dat <- read.csv("/Users/guyaridor/Desktop/recommender_systems/rec_sys_conf_paper/time_path_25.csv")
+time_dat <- time_dat %>% mutate(formatted_regime = ifelse(regime == "rec", "Omniscient", ifelse(regime == "no_rec", "No Rec", "Partial")))
+time_dat <- time_dat %>% mutate(local_move = as.numeric(consumption_dist < 10))
+
+g <- ggplot(time_dat, aes(x=t, y=local_move)) +
+    geom_smooth(aes(colour=formatted_regime)) +
+    labs(x="t", y="Fraction of Local Search"
+    )
+if(use_hrbrthemes){
+  g <- g + theme_ipsum_rc()
+}
 ggsave(
-  filename="/Users/ssikdar/Downloads/econ/bubbles/figures/welfare_diversity_scatter_bin.jpeg", 
-  plot=scatter(rec_data, "diversity_score", "welfare", use_hrbrthemes)
+  filename=paste(WORKING_DIR, "figures/local_moves_25.jpeg", sep=""), 
+  plot=g
 )
+
+partial <- filter(time_dat, regime == "partial")
+partial$follow_recommendation <- as.numeric(partial$follow_recommendation) - 1
+g <- ggplot(partial, aes(x=t, y=follow_recommendation)) +
+  geom_smooth() +
+  labs(x="t", y="Fraction Following Recommendation"#, title="Recommendation Effectiveness"
+  )
+if(use_hrbrthemes){
+  g <- g + theme_ipsum_rc(plot_title_size = 24, axis_title_size = 14)
+}
+ggsave(
+  filename=paste(WORKING_DIR, "figures/rec_obedience_25.jpeg", sep=""), 
+  plot=g
+)
+
+
